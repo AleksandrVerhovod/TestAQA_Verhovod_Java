d8ec0a2b1a604b7a17cef7447cfa12630b123f68
Aleksandr <verhovod.aleksandr@gmail.com>
2022-12-13 23:21:02 +0300

    task2 added

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Task1 task1 = new Task1();
         task1.task_1();
+        Task2 task2 = new Task2();
+        task2.task_2();
 
     }
 }
--- src/Task2.java
+++ src/Task2.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class Task2 {
+    //    Составить алгоритм: если введенное имя совпадает с Вячеслав,
+//    то вывести “Привет, Вячеслав”, если нет, то вывести "Нет такого имени"
+    public void task_2() {
+        String name = "";
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите имя, учитывая регистр символов:");
+        if (input.hasNextLine()) {
+            name = input.nextLine();
+            if (name.equals("Вячеслав")) {
+                System.out.println("Привет, " + name);
+            } else {
+                System.out.println("Нет такого имени");
+            }
+        }
+    }
 }
